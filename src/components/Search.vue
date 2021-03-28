687a015f534f22a701ef4f1d8b8323c3c6eeb026
jalan116 <jalan116@stu.southtexascollege.edu>
2021-03-28 18:03:56 -0500

    GIF FinderApp

---
+++ src/components/Search.vue
@@ -0,0 +1,11 @@
+<template>
+    <input class="container block mx-auto rounded-full mt-8 p-2 text-2xl 
+    outline-none text-center focus:ring-4 foucs:ring-green-500 focus:ring-opacity-50" 
+    type="text" placeholder="I'm looking for ..." 
+    v-model="searchItem"
+    @change="getGifs"/>
+</template>
+
+<script setup>
+import { searchItem, getGifs} from '../helpers/useGifs'
+</script>
